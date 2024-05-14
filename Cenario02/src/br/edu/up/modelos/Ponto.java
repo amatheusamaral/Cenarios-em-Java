f883d788f17e25f0a5d67615f7078755f2e5255e
AngeloBalchaki <augustoangelo041@gmail.com>
2024-05-14 16:14:54 -0300

    Commit de Entrega

--- Cenario02/src/br/edu/up/modelos/Ponto.java
+++ Cenario02/src/br/edu/up/modelos/Ponto.java
@@ -4,19 +4,16 @@ public class Ponto {
     private double x;
     private double y;
 
-    // Construtor para criar um ponto na origem (0,0)
     public Ponto() {
         this.x = 0;
         this.y = 0;
     }
 
-    // Construtor para criar um ponto nas coordenadas (x, y)
     public Ponto(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    // Métodos getters para obter os valores de x e y
     public double getX() {
         return x;
     }
@@ -25,7 +22,6 @@ public class Ponto {
         return y;
     }
 
-    // Métodos setters para alterar os valores de x e y
     public void setX(double x) {
         this.x = x;
     }
@@ -34,7 +30,6 @@ public class Ponto {
         this.y = y;
     }
 
-    // Método para calcular a distância entre dois pontos
     public double calcularDistancia(Ponto outroPonto) {
         double distanciaX = this.x - outroPonto.getX();
         double distanciaY = this.y - outroPonto.getY();
--- Cenario02/src/br/edu/up/modelos/Programa.java
+++ Cenario02/src/br/edu/up/modelos/Programa.java
@@ -2,25 +2,19 @@ package br.edu.up.modelos;
 
 public class Programa {
     public static void main(String[] args) {
-        // 1. Crie um objeto ponto1 usando o primeiro construtor;
         Ponto ponto1 = new Ponto();
 
-        // 2. Crie um objeto ponto2 na posição (2,5);
         Ponto ponto2 = new Ponto(2, 5);
 
-        // 3. Calcule a distância do ponto1 ao ponto2;
         double distanciaP1P2 = ponto1.calcularDistancia(ponto2);
         System.out.println("Distância do ponto1 ao ponto2: " + distanciaP1P2);
 
-        // 4. Calcule a distância do ponto2 às coordenadas (7,2);
         Ponto ponto3 = new Ponto(7, 2);
         double distanciaP2P3 = ponto2.calcularDistancia(ponto3);
         System.out.println("Distância do ponto2 às coordenadas (7,2): " + distanciaP2P3);
 
-        // 5. Altere o valor de x para 10 no ponto1;
         ponto1.setX(10);
 
-        // 6. Altere o valor de y para 3 no ponto1;
         ponto1.setY(3);
     }
 }
